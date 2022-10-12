03271f32aab6ed5d945aadb86a1987e58363b450
Riley Tomasek <riley.tomasek@gmail.com>
2022-10-12 15:31:03 -0400

    Add og:image and favicon

--- main.tsx
+++ main.tsx
@@ -9,9 +9,11 @@ import "https://esm.sh/prismjs@1.27.0/components/prism-python";
 blog({
   title: "Dev Tips",
   description: "Snippets, tools, and techniques for development.",
-  avatar: "./logo.png",
-  avatarClass: "rounded-full",
   author: "Riley Tomasek",
+  favicon: "https://assets.rile.yt/favicons/favicon.svg",
+  avatar: "https://assets.rile.yt/logos/circle-128.svg",
+  avatarClass: "rounded-full",
+  ogImage: "https://assets.rile.yt/api/default",
   links: [
     { title: "Twitter", url: "https://twitter.com/rileytomasek" },
     { title: "GitHub", url: "https://github.com/rileytomasek" },
